b85321e6f9788336471974a120ab4433f107eeae
LordofSabres <79553533+LordofSabres@users.noreply.github.com>
2023-08-24 18:42:15 -0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /home/pi/Desktop/tjbot/recipes/conversation/
+sudo node conversationx.js
---
+++ test1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /home/pi/Desktop/tjbot/recipes/sarcasm/
+sudo node conversation4.js
